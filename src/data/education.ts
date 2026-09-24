64abc9ff7efcc8a585034bbe4fd8c0154c9cc26b
nithinpolepaka <nithinpolepaka@gmail.com>
2026-09-25 01:30:33 +0530

    Personalize education section

--- src/data/education.ts
+++ src/data/education.ts
@@ -2,36 +2,24 @@ import { type ExperienceShowcaseListItemProps } from "@/components/experience/ex
 
 export const EDUCATION: ExperienceShowcaseListItemProps[] = [
   {
-    title: "Bachelor Of Engineering in Electronics and Telecommunication",
+    title: "M.Tech, Computer Science",
     organisation: {
-      name: "Rizvi College Of Engineering",
-      href: "https://eng.rizvi.edu.in/",
+      name: "Vaagdevi College of Engineering",
+      href: "#",
     },
-    date: "2019-2023",
-    location: "Bandra, Mumbai",
+    date: "2024 - 2026 (Expected)",
+    location: "Telangana, India",
     description:
-      "Relevant courses included Data Structures and Algorithms, Web Design, Cloud Computing, Artificial Neural Network and Database Management.",
+      "Pursuing M.Tech in Computer Science while working full-time as a Python Developer.",
   },
   {
-    title: "Higher Secondary School",
+    title: "B.Tech, Cyber Security",
     organisation: {
-      name: "Mithibai College",
-      href: "https://www.mithibai.ac.in/",
+      name: "Institute of Aeronautical Engineering (IARE)",
+      href: "https://www.iare.ac.in/",
     },
-    date: "2016-2018",
-    location: "Vile Parle, Mumbai",
-    description:
-      "Completed the course in Electronics Bifocal Science with 68%. Started freelancing making 3D assets for games. Got interested in game development and learned unity and c#",
-  },
-  {
-    title: "Secondary School Education",
-    organisation: {
-      name: "Holy Family High School",
-      href: "https://holyfamilyandheri.org/",
-    },
-    date: "2017-2018",
-    location: "Andheri, Mumbai",
-    description:
-      "Learned good values,dedication and disciplne. Played district football for 5 years.",
+    date: "June 2020 - June 2024",
+    location: "Hyderabad, India",
+    description: "Bachelor of Technology in Cyber Security.",
   },
 ];
